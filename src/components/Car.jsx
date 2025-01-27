13d90ee2214689769a2e957fd26b528e1fc2d25b
Henrik <henaa@stud.ntnu.no>
2025-01-27 15:18:51 +0100

    SVG logo and removed legacy flask code

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -5,3 +5,4 @@ const Car = () => {
         </article>
     );
 }
+export default Car;
---
+++ src/components/Filters.jsx
@@ -0,0 +1,9 @@
+const filters = () => {
+    return (
+        <div>
+            <div className={filterHeader}
+            <h2>Car</h2>
+        </div>
+    );
+}
+export default filters;
