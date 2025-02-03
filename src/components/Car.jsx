ec151d7657fa070923e46aeee2c87a597a4246e8
Henrik Aamot <henrik.aamot@live.no>
2025-02-03 13:45:11 +0100

    Changes to test list

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -1,18 +1,19 @@
 import "../static/css/car.css";
 
 const Car = () => {
+    const features = ["Warming seats", "Roof window", "Floor windows"];
+
     return (
         <article className="car">
             <h2>Car name (Manufacturer + Model)</h2>
             <img src="https://www.shutterstock.com/image-vector/car-logo-icon-emblem-design-600nw-473088025.jpg" alt="Image of car in article" />
             <h3>Features</h3>
             <ul>
-                <li>
-                    Warming seats
-                </li>
-                <li>
-                    Roof window
-                </li>
+                {features.map((feature, index) => (
+                    <li key={index}>
+                        {feature}
+                    </li>
+                ))}
             </ul>
         </article>
     );
