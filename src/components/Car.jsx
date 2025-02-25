b888068e07d58f26ac681846293df7a6c6ab2574
Henrik <henaa@stud.ntnu.no>
2025-02-25 13:32:42 +0100

    Car proptype now according to json sent by backend

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -11,8 +11,8 @@ function Car ({car}) {
             <img src={`https://dummyimage.com/600x200/000/ffffff&text=<img src="${car.manufacturer}+${car.carModel}+(${car.productionYear})" />`} alt={car.carModel}/>
             <div>
                 <div>
-                    <p><strong>Provider: </strong>{}</p>
-                    <p><strong>Price: </strong>{}</p>
+                    <p><strong>Provider: </strong>{car.user}</p>
+                    <p><strong>Price: </strong>{car.price}</p>
                     <p><strong>Number of seats: </strong>{car.numberOfSeats}</p>
                     <p><strong>Transmission: </strong>{car.transmissionType}</p>
                     <p><strong>Fuel: </strong>{car.fuelType}</p>
@@ -28,11 +28,15 @@ function Car ({car}) {
 Car.propTypes = {
     car: PropTypes.shape({
         id: PropTypes.number,
+        imagePath: PropTypes.string,
         carModel: PropTypes.string,
+        price: PropTypes.number,
         numberOfSeats: PropTypes.number,
         productionYear: PropTypes.number,
         manufacturer: PropTypes.string,
         transmissionType: PropTypes.string,
+        carStatus: PropTypes.string,
+        user: PropTypes.string,
         fuelType: PropTypes.string,
         features: PropTypes.array
     })
