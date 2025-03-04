ada9ac7f7cff89c4a5c8e0d50cea6f4ec0dc2d46
Henrik <henaa@stud.ntnu.no>
2025-03-04 08:15:32 +0100

    Initial commit for orders

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,14 @@
+
+
+
+//TODO: Fetch the orders from the database and display them in a table
+function Orders() {
+  return (
+    <div>
+      <h1>Orders</h1>
+
+    </div>
+  );
+}
+
+export default Orders;
