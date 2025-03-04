f9e6cea2de53157ad184ba9092363f3395e6a5d0
Henrik <henaa@stud.ntnu.no>
2025-03-04 09:35:20 +0100

    Cleaned up CarList

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,5 +1,4 @@
 import Car from "./Car.jsx";
-import PropTypes from 'prop-types';
 
 function CarList (cars) {
     cars = cars["cars"]
@@ -9,25 +8,6 @@ function CarList (cars) {
         </div>
     );
 }
-
-CarList.propTypes = {
-    cars: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number,
-            imagePath: PropTypes.string,
-            carModel: PropTypes.string,
-            price: PropTypes.number,
-            numberOfSeats: PropTypes.number,
-            productionYear: PropTypes.number,
-            manufacturer: PropTypes.string,
-            transmissionType: PropTypes.string,
-            carStatus: PropTypes.string,
-            user: PropTypes.string,
-            fuelType: PropTypes.string,
-            features: PropTypes.array
-        })
-    )
-};
 
 
 export default CarList;
