40edb8eb2d4d5b020a9ab153bab8f9fac5331b0c
Henrik Aamot <50317702+Nezerpz@users.noreply.github.com>
2025-04-28 13:11:48 +0200

    Edited images and added a few

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -15,7 +15,7 @@ function Car ({car}) {
         <div className="car">
             <h3>{car.manufacturer} {car.carModel} ({car.productionYear})</h3>
             <img src={`https://dummyimage.com/600x200/000/ffffff&text=<img src="${car.manufacturer}+${car.carModel}+(${car.productionYear})" />`} alt={car.carModel}/>
-            <img src={car.imagePath} alt={car.carModel}/>
+            <img src={"src" + car.imagePath} alt={car.carModel}/>
             {console.log(car.imagePath)}
             <div>
                 <div>
