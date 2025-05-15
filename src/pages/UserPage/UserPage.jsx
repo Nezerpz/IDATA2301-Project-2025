9d486f25edf1a14f041723857c16a4f4b53ac440
ekroll <ekroll@pm.me>
2025-05-15 11:27:58 +0200

    add back unsuspend

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -39,6 +39,8 @@ async function updatePassword(event, user) {
     }
     else {
         console.log(response)
+    }
+}
 
 async function unsuspendUser(user) {
     if (!confirm("Sure you want to unsuspend this user?")) return
@@ -119,14 +121,26 @@ function renderPage(user, userModification, setUserModification) {
                 <button className={"big-button"}
                     onClick={(e) => updatePassword(e, user)}>Send password reset</button>
             </div>
-            <h4>Suspend or delete user?</h4>
+            <h4>Suspend/unsuspend or delete user?</h4>
             <div className={"flex-container-row"}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                     stroke="currentColor" className="size-6 flex-end-item" width={48}
-                     onClick={() => suspendUser(user)}>
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" viewBox="0 0 24 24" 
+                    strokeWidth={1.5} stroke="currentColor" 
+                    width={48}
+                    className="size-6" onClick={() => unsuspendUser(user)}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
+                </svg>
+
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" viewBox="0 0 24 24" 
+                    strokeWidth="1.5" stroke="currentColor" 
+                    className="size-6 flex-end-item" width={48}
+                    onClick={() => suspendUser(user)}>
                     <path strokeLinecap="round" strokeLinejoin="round"
                           d="M14.25 9v6m-4.5 0V9M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
+
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                      stroke="currentColor" className="size-6 flex-end-item" width={48}
                      onClick={() => deleteUser(user)}>
